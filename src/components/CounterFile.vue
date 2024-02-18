fb2f3a612df34db28808ec68ae4a1c03347760de
alexisEdu2004 <max34junior@gmail.com>
2024-02-17 22:32:41 -0600

    update

---
+++ src/components/CounterFile.vue
@@ -0,0 +1,36 @@
+<template>
+    <h1>{{ counter }}</h1>
+    <button @click="incremento">+</button>
+    <button @click="reset">reset</button>
+    <button @click="decremento">-</button>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+
+     export default{
+        setup(){
+            const counter = ref(0);
+            return {counter};
+        },
+
+
+
+
+        methods: {
+            incremento(){
+                this.counter++;
+            },
+
+            reset(){
+                this.counter = 0;
+            },
+
+            decremento(){
+                this.counter--;
+            }
+        }
+     }
+    
+</script>
